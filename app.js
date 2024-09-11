238027b364389a85dd93651b0e9b0610ea337fcd
thunderbirdgit <gangadharan.om@gmail.com>
2024-09-11 20:06:38 +0530

    update url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Replace the following with your MongoDB connection string
-const uri = 'mongodb://a6f1f94094a0a44029f88f3ce887a24b-773113474.us-west-2.elb.amazonaws.com:27017'; // Update this if using a different host or port
+const uri = 'mongodb://a6df0b325c4204ae996b2fcba2d2735b-1857915018.us-west-2.elb.amazonaws.com:27017'; // Update this if using a different host or port
 const dbName = 'helloworld';
 
 app.get('/', async (req, res) => {
